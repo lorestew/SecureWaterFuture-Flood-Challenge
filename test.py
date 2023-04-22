31dee2184dbd20c7e5658df4c819aa43539c98a5
richomielorenzo <derpyspiderman@gmail.com>
2023-04-22 01:52:06 -0700

    updated input

--- test.py
+++ test.py
@@ -11,37 +11,21 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 
-#zip_path = tf.keras.utils.get_file(
-#    fname=r'data/BearCreek_McKee_flow.csv',
-#    origin=None,
-#    extract=False)
-#csv_path, _ = os.path.splitext(zip_path)
+location = "file:///Users/Derpy/Documents/SecureWaterFuture/SWFdata/BearCreek_precipitation_future.csv"
 
-#df = pd.read_csv(csv_path)
 
-#csv_path = "data/BearCreek_McKee_flow.csv"
-#stream_train = pd.read_csv(
-#    csv_path,
-#    names = ["date", "stage_height", "streamflow"])
-#stream_train.head()
-
-location = "file://C:/Users/Derpy/Documents/SecureWaterFuture/SWFdata/BearCreek_McKee_flow.csv"
-path = tf.keras.utils.get_file(
-    fname="BearCreek_McKee_flow",
-    origin=location)
-
-stream_train_tf = tf.data.experimental.make_csv_dataset(
-    path,
-    batchsize = 10,
-    num_epochs=1,
-    ignore_errors=True
+train = pd.read_csv(
+    location,
+    names=["Date", "Stage Height", "Streamflow"]
 )
 
-for batch, label in stream_train_tf.take(1):
-    for key, value in batch.items():
-        print(f"{key:10s}: {value}")
+print(train.head())
 
-
+#dates = pd.to_datetime(train.pop(
+#    "Date",
+#    format ='%d.%m.%Y'))
+#
+#print(train.head())
 
 
 
